07acb3b4d1a177982d4f96a391a5a0ef9cd5bf31
SreeramThatavarthi <56222315+SreeramThatavarthi@users.noreply.github.com>
2020-08-30 14:54:15 +0530

    Updates

---
+++ precache-manifest.054ed3319f62d71381134e33551ca06c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c572bfbf69d4da75951e0829e9ae683a",
+    "url": "/colorPicker/index.html"
+  },
+  {
+    "revision": "946d189d37735ecfaddf",
+    "url": "/colorPicker/static/css/2.fe7cd3c1.chunk.css"
+  },
+  {
+    "revision": "c21582a620e65b0e8c28",
+    "url": "/colorPicker/static/css/main.ce7a5b41.chunk.css"
+  },
+  {
+    "revision": "946d189d37735ecfaddf",
+    "url": "/colorPicker/static/js/2.add939bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/colorPicker/static/js/2.add939bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21582a620e65b0e8c28",
+    "url": "/colorPicker/static/js/main.8983ee8d.chunk.js"
+  },
+  {
+    "revision": "0ea632c81c7c9a409836",
+    "url": "/colorPicker/static/js/runtime-main.a5d0f2d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/colorPicker/static/media/logo.5d5d9eef.svg"
+  }
+]);
